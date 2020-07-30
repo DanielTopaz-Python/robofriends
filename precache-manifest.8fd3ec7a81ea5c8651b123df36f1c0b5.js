0897de3a0dc0557ff7e548a298f2309d11db681b
Daniel Topaz <danieltopaz.python@gmail.com>
2020-07-30 21:07:10 +0300

    Updates

---
+++ precache-manifest.8fd3ec7a81ea5c8651b123df36f1c0b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebc593127a074ad5cd4578a07c1172d3",
+    "url": "/DanielTopaz-Python/robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/DanielTopaz-Python/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "9372a57a48facfe7d7d8",
+    "url": "/DanielTopaz-Python/robofriends/static/css/main.b88d7b27.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/DanielTopaz-Python/robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DanielTopaz-Python/robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9372a57a48facfe7d7d8",
+    "url": "/DanielTopaz-Python/robofriends/static/js/main.cb5f113b.chunk.js"
+  },
+  {
+    "revision": "c8c3f375d7807ec0f6e4",
+    "url": "/DanielTopaz-Python/robofriends/static/js/runtime-main.1872b7ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/DanielTopaz-Python/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
